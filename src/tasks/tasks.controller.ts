2be6a8f55a1ba18f167157fb475f8a51ba58f314
Avinash Pandey <avinash.12009084@lpu.in>
2024-06-05 09:31:28 +0530

    Turned off all eslint errors

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
@@ -9,5 +11,13 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTasks(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ) {
+    console.log('body', body);
   }
 }
